dc4c98e90e8c1ee96fe1ed895d6793120190ca8d
MounishRamesh <mounishr6680@gmail.com>
2025-02-26 11:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (75.79%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.java
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -7,17 +7,17 @@ class Solution {
             return mat;
         }
 
-        int row_index = 0; 
-        int col_index = 0;
+        int nc = 0; 
+        int nr = 0;
 
         int[][] res = new int[r][c];
         for(int i =0 ; i < row; i++){
             for(int j = 0; j < column; j++){
-                res[row_index][col_index]=mat[i][j];
-                col_index++;
-                if(col_index == c){
-                    col_index=0;
-                    row_index++;
+                res[nr][nc]=mat[i][j];
+                nc++;
+                if(nc == c){
+                    nc=0;
+                    nr ++;
                 }
             }
         }
