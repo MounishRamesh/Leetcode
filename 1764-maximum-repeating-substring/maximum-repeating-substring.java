fe350dd4106dadff46972b279852751921a50659
MounishRamesh <mounishr6680@gmail.com>
2025-03-05 22:40:43 +0530

    Time: 1 ms (60.91%) | Memory: 41.9 MB (44.28%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int count = 0 ;
+        String s = word ;
+        while(sequence.contains(s)){
+            count ++ ;
+            s = s + word ;
+        }
+        return  count ; 
+        
+    }
+}
