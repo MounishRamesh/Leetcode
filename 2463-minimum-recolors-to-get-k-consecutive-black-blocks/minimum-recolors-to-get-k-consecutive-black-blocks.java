412896ccd6b8166fa712486f67141e5094d772bc
MounishRamesh <mounishr6680@gmail.com>
2025-03-08 22:43:41 +0530

    Time: 1 ms (92.10%) | Memory: 41.8 MB (18.39%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,33 +1,22 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int count = 0 ;
-        for(int i = 0 ; i < k ; i ++){
+        int min = 0 ;
+        int ans = Integer.MAX_VALUE ;
+        for(int i = 0 ;i < k ;i++){
             if(blocks.charAt(i) == 'W'){
-                count ++ ;
+                min ++ ;
             }
         }
-        int max = count ;
-        int j = 0 ;
-        int k1 = k ;
-        int k2 = k ;
-        while(k2 < blocks.length()){
-            int sum = 0 ;
-            int sum2 = 0 ;
-            j ++ ;
-            for(int i = j ; i < k2+1 ; i ++){
-                if(blocks.charAt(i) == 'W'){
-                    sum ++ ;
-                }
-                else if(blocks.charAt(i) == 'B'){
-                    sum2 ++ ;
-                }
+        ans = min ;
+        for(int i = k ; i < blocks.length() ; i ++){
+            if(blocks.charAt(i-k) == 'W'){
+                ans -- ;
             }
-            if(sum2 >= k){
-                return 0 ;
+            if(blocks.charAt(i) == 'W'){
+                ans ++ ;
             }
-            max = Math.min(max , sum) ;
-            k2 ++ ;
+            min = Math.min(ans, min) ;
         }
-        return max ;
+        return min ;
     }
 }
