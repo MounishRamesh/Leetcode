5e66513857e745147109db18c723ec1c7e9715c5
MounishRamesh <mounishr6680@gmail.com>
2025-03-27 23:00:58 +0530

    Time: 24 ms (18.80%) | Memory: 50 MB (18.98%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,38 +1,23 @@
-// class Solution {
-//     public int numSubarraysWithSum(int[] nums, int goal) {
-//         HashMap<Integer,Integer> hs = new HashMap<>() ;
-//         int count = 0 ;
-//         int sum = 0 ;
-//         for(int i = 0 ; i< nums.length ;i ++){
-//             sum = sum + nums[i] ;
-//             if(sum == goal){
-//                 count ++ ;
-//             }
-//             if(hs.containsKey(sum - goal)){
-//                 count += hs.get(sum - goal) ;
-//             }
-//             if(hs.containsKey(sum)){
-//                 hs.put(sum , hs.get((sum)) + 1) ;
-//             }
-//             else{
-//                 hs.put(sum , 1) ;
-//             }
-//         }
-//         return count ;
-//     }
-// }
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length;
-        int result = 0;
-        int prefixSum = 0;
-        Map<Integer,Integer> map = new HashMap();
-        map.put(0,1);
-        for(int i=0; i<n; i++){
-            prefixSum += nums[i];
-            result += map.getOrDefault(prefixSum - goal, 0);
-            map.put(prefixSum, map.getOrDefault(prefixSum,0) + 1);
+        HashMap<Integer,Integer> hs = new HashMap<>() ;
+        int count = 0 ;
+        int sum = 0 ;
+        for(int i = 0 ; i< nums.length ;i ++){
+            sum = sum + nums[i] ;
+            if(sum == goal){
+                count ++ ;
+            }
+            if(hs.containsKey(sum - goal)){
+                count += hs.get(sum - goal) ;
+            }
+            if(hs.containsKey(sum)){
+                hs.put(sum , hs.get((sum)) + 1) ;
+            }
+            else{
+                hs.put(sum , 1) ;
+            }
         }
-        return result;
+        return count ;
     }
 }
