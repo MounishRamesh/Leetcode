f16468987cb5e28e9d083cd144c518f6c159619f
MounishRamesh <mounishr6680@gmail.com>
2025-03-31 15:31:43 +0530

    Time: 58 ms (30.31%) | Memory: 45.6 MB (12.24%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,29 +1,46 @@
+// class Solution {
+//     public int longestSubstring(String s, int k) {
+//         if(s.length() == 0 || s.length() < k){
+//             return 0 ;
+//         }
+//         HashMap<Character,Integer > hs = new HashMap<>() ;
+//         for(int i =0 ; i < s.length() ;i ++){
+//             char ch = s.charAt(i) ;
+//             if(hs.containsKey(ch)){
+//                 hs.put(ch , hs.get(ch) + 1) ;
+//             }
+//             else{
+//                 hs.put(ch , 1) ;
+//             }
+//         }
+//         for(char ch : hs.keySet()){
+//             if(hs.get(ch) < k){
+//                 int min =0 ;
+//                 String[] arr = s.split(String.valueOf(ch)) ;
+//                 for(String val : arr){
+//                     int len = longestSubstring(val,k) ;
+//                     min = Math.max(min , len) ;
+//                 }
+//                 return min ;
+//             }
+//         }
+//         return s.length() ;
+//     }
+// }
 class Solution {
     public int longestSubstring(String s, int k) {
-        if(s.length() == 0 || s.length() < k){
-            return 0 ;
+        int freq [] = new int [26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
         }
-        HashMap<Character,Integer > hs = new HashMap<>() ;
-        for(int i =0 ; i < s.length() ;i ++){
-            char ch = s.charAt(i) ;
-            if(hs.containsKey(ch)){
-                hs.put(ch , hs.get(ch) + 1) ;
-            }
-            else{
-                hs.put(ch , 1) ;
-            }
-        }
-        for(char ch : hs.keySet()){
-            if(hs.get(ch) < k){
-                int min =0 ;
-                String[] arr = s.split(String.valueOf(ch)) ;
-                for(String val : arr){
-                    int len = longestSubstring(val,k) ;
-                    min = Math.max(min , len) ;
-                }
-                return min ;
+        int max =0;
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']<k&&freq[s.charAt(i)-'a']>0){
+                int right =longestSubstring(s.substring(0,i),k);
+                int left = longestSubstring(s.substring(i+1),k);
+                return  max = Math.max(right,left);
             }
         }
-        return s.length() ;
+        return s.length();
     }
 }
