f88929e1628b60f147ae077f3c8a417a874b0e13
MounishRamesh <mounishr6680@gmail.com>
2025-03-31 16:37:43 +0530

    Time: 76 ms (31.23%) | Memory: 59.5 MB (80.43%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,24 +1,25 @@
+import java.util.HashMap;
+
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        int i1 = 0 ;
-        int count = 0 ;
-        HashMap<Integer , Integer > hs = new HashMap<>() ;
-        for(int i = 0 ;i < nums.length; i ++){
-            if(hs.containsKey(nums[i])){
-                hs.put(nums[i] , hs.get(nums[i])  +1) ;
-            }
-            else{
-                hs.put(nums[i],1) ;
-            }
-            while(hs.get(nums[i])> k){
-                hs.put(nums[i1] , hs.get(nums[i1])-1) ;
-                if(hs.get(nums[i1])== 0){
-                    hs.remove(nums[i1]) ;
+        int i1 = 0;
+        int count = 0;
+        HashMap<Integer, Integer> hs = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            hs.put(nums[i], hs.getOrDefault(nums[i], 0) + 1);
+
+            while (hs.get(nums[i]) > k) {
+                hs.put(nums[i1], hs.get(nums[i1]) - 1);
+                if (hs.get(nums[i1]) == 0) {
+                    hs.remove(nums[i1]);
                 }
-                i1 ++ ;
+                i1++;
             }
-            count = Math.max(count ,i-i1+1) ; 
+
+            count = Math.max(count, i - i1 + 1);
         }
-        return count ;
+
+        return count;
     }
 }
