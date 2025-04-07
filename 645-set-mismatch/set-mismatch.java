1504a4063976a5ba22c6788c3bfc77e4493644c7
MounishRamesh <mounishr6680@gmail.com>
2025-04-07 11:17:20 +0530

    Time: 3 ms (80.61%) | Memory: 45.8 MB (36.80%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] arr = new int[2] ;
-        HashMap<Integer,Integer> hs = new HashMap<>() ;
-        for(int i = 0 ;i < nums.length; i ++){
-            if(hs.containsKey(nums[i])){
-                arr[0] = nums[i] ;
-                break ;
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=nums[nums[i]-1])swap(nums,nums[i]-1,i);
+            //else if(i>nums[i]-1)return new int[]{nums[i],i+1};
+            else i++;
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]-1!=i){
+                return new int[]{nums[i],i+1};
             }
-            else{
-                hs.put(nums[i] , 1) ;
-            }
-         }
-         int sum = 0 ;
-         int sum1 = 0; 
-         for(int i : nums){
-            sum = sum + i ;
-         }
-         sum = sum - arr[0];
-         for(int i = 1 ;i <= nums.length; i++){
-            sum1 += i ;
-         }
-         arr[1] = sum1-sum ;
-         return arr ;
+        }
+        return new int[]{-1,-1};
+    }
+    public static void swap(int[] nums,int i,int j){
+        if(i!=j){
+            nums[i]=nums[i]+nums[j];
+            nums[j]=nums[i]-nums[j];
+            nums[i]=nums[i]-nums[j];
+        }
     }
 }
