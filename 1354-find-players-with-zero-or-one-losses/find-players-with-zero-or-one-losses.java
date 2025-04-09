dee0e235370a6196036c25983cf58a8fd060b5b5
MounishRamesh <mounishr6680@gmail.com>
2025-04-09 11:30:31 +0530

    Time: 42 ms (92.82%) | Memory: 107.6 MB (24.07%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,43 +1,29 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        HashMap<Integer , Integer > hs = new HashMap<>() ;
-        HashMap<Integer , Integer > hs1 = new HashMap<>() ;
         List<List<Integer>> ls = new ArrayList<>() ;
+        ArrayList<Integer> win = new ArrayList<>() ;
+        ArrayList<Integer> loss = new ArrayList<>() ;
+        int[] arr1 = new int[100001] ;
+        int[] arr2 = new int[100001] ;
         int n = matches.length ;
-        for(int i = 0 ;i < n ;i ++){
-            int val = matches[i][0] ;
-            if(hs.containsKey(val)){
-                hs.put(val , hs.get(val )+1) ;
-            }
-            else{
-                hs.put(val , 1) ;
+        for(int i = 0 ;i< n ;i++){
+            arr1[matches[i][0]] ++ ;
+        }
+        for(int i = 0 ;i< n ;i++){
+            arr2[matches[i][1]] ++ ;
+        }
+        for(int i = 0 ;i < arr1.length ;i ++){
+            if(arr1[i] >= 1 && arr2[i] == 0){
+                win.add(i) ;
             }
         }
-        for(int i = 0 ;i < n ;i ++){
-            int val = matches[i][1] ;
-            if(hs1.containsKey(val)){
-                hs1.put(val , hs1.get(val )+1) ;
-            }
-            else{
-                hs1.put(val , 1) ;
+        ls.add(win) ;
+        for(int i = 0 ;i < arr1.length ;i ++){
+            if(arr2[i] == 1){
+                loss.add(i) ;
             }
         }
-        List<Integer> ls1 = new ArrayList<>() ;
-        for(int i : hs.keySet()){
-            if(!hs1.containsKey(i)){
-                ls1.add(i) ;
-            }
-        }
-        Collections.sort(ls1) ;
-        ls.add(ls1) ;
-        List<Integer> ls2 = new ArrayList<>() ;
-        for(int j : hs1.keySet()){
-            if(hs1.get(j) == 1){
-                ls2.add(j) ;
-            }
-        }
-        Collections.sort(ls2) ;
-        ls.add(ls2); 
+        ls.add(loss) ;
         return ls ;
     }
 }
