d530f9f0dcfa7891b6fa868bd13dcf3ce654722d
MounishRamesh <mounishr6680@gmail.com>
2025-04-11 10:02:51 +0530

    Time: 25 ms (90.62%) | Memory: 44.9 MB (32.32%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.java
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -1,38 +1,23 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
         int count = 0 ;
-        for(int i = low ; i <= high ;i ++ ){
-            if(digits(i) == true){
+        for(int i = low ; i <= high; i ++){
+            String s = String.valueOf(i) ;
+            int n = s.length() ;
+            if(n%2 != 0){
+                continue ;
+            }
+            int half = n/2 ;
+            int count1 = 0 ;
+            int count2 = 0 ;
+            for(int i1 = 0 ;i1 < half ;i1 ++){
+                count1 += s.charAt(i1) ;
+                count2 += s.charAt(half + i1) ;
+            }
+            if(count1 == count2){
                 count ++ ;
             }
         }
         return count ;
-    }
-    public static boolean digits(int i){
-        int a = (int) Math.log10(i)+1 ;
-        if(a % 2 != 0){
-            return false ;
-        }
-        int[] arr = new int[a] ;
-        int j = a-1 ;
-        while(i > 0){
-            int rem = i % 10 ;
-            arr[j] = rem ;
-            i /= 10 ;
-            j -- ;
-        }
-        int count1 = 0 ;
-        int count2 = 0 ;
-        int n = a /2 ;
-        for(int k = 0 ;k < n ;k ++){
-            count1 += arr[k] ;
-        }
-        for(int k = n ;k < arr.length; k ++){
-            count2 += arr[k] ;
-        }
-        if(count1 == count2){
-            return true ;
-        }
-        return false ;
     }
 }
