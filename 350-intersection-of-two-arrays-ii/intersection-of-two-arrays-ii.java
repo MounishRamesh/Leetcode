641209ab569548d7286cc00045eddbc7f1c94969
MounishRamesh <mounishr6680@gmail.com>
2025-04-12 11:45:07 +0530

    Time: 4 ms (35.02%) | Memory: 43.3 MB (56.78%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer>ls = new ArrayList<>() ;
+        HashMap<Integer ,Integer > hs = new HashMap<>() ;
+        for(int  i : nums1){
+            if(hs.containsKey(i)){
+                hs.put(i , hs.get(i) + 1) ;
+            }
+            else{
+                hs.put(i , 1) ;
+            }
+        }
+        for(int i  : nums2){
+            if(hs.containsKey(i)){
+                hs.put( i ,hs.get(i) - 1) ;
+                ls.add(i) ;
+                if(hs.get(i) == 0){
+                    hs.remove(i) ;
+                }
+            }
+        }
+        int[] arr = new int[ls.size()] ;
+        for(int i =0  ;i < arr.length ;i ++){
+            arr[i] = ls.get(i) ;
+        }
+        return arr; // placeholder
+    
+    }
+}
