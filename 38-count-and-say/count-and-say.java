fd46c70fd912e0adf7da197330348192ae927a9d
MounishRamesh <mounishr6680@gmail.com>
2025-04-18 19:49:10 +0530

    Time: 1 ms (99.58%) | Memory: 41.4 MB (60.37%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,32 +1,61 @@
+// class Solution {
+//     public String countAndSay(int n) {
+//         StringBuilder sb = new StringBuilder() ;
+//         int a = 1 ;
+//         String s = "1" ;
+//         for(int i = 1 ;i < n ; i ++){
+//             s = countandsay(s) ;
+//             System.out.print(s + " ") ;
+//         }
+//         return s ;
+//     }
+//     public static String countandsay(String s){
+//         int n = s.length() ;
+//         StringBuilder sb = new StringBuilder() ;
+//         int count = 1 ;
+//         for(int i = 1 ;i < n ;i ++){
+//             char ch = s.charAt(i) ;
+//             char ch1 = s.charAt(i-1) ;
+//             if(ch == ch1){
+//                 count ++ ;
+//             }
+//             else{
+//                 sb.append(Integer.toString(count)) ;
+//                 sb.append(Character.toString(s.charAt(i-1))) ;
+//                 count = 1 ;
+//             }
+//         }
+//         sb.append(Integer.toString(count)) ;
+//         sb.append(Character.toString(s.charAt(n-1))) ;
+//         return sb.toString() ; 
+//     }
+// }
+
 class Solution {
     public String countAndSay(int n) {
-        StringBuilder sb = new StringBuilder() ;
-        int a = 1 ;
-        String s = "1" ;
-        for(int i = 1 ;i < n ; i ++){
-            s = countandsay(s) ;
-            System.out.print(s + " ") ;
+        String s = "1";
+        for(int i=2; i<= n; i++){
+            s = countandAdd(s);
         }
-        return s ;
+        return s;
     }
-    public static String countandsay(String s){
-        int n = s.length() ;
-        StringBuilder sb = new StringBuilder() ;
-        int count = 1 ;
-        for(int i = 1 ;i < n ;i ++){
-            char ch = s.charAt(i) ;
-            char ch1 = s.charAt(i-1) ;
-            if(ch == ch1){
-                count ++ ;
+    String countandAdd(String s){
+        StringBuilder currString = new StringBuilder();
+        char ch = s.charAt(0);
+        int count = 1;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i) == ch){
+                count++;
             }
             else{
-                sb.append(Integer.toString(count)) ;
-                sb.append(Character.toString(s.charAt(i-1))) ;
-                count = 1 ;
+                currString.append(count);
+                currString.append(ch);
+                ch = s.charAt(i);
+                count = 1;
             }
         }
-        sb.append(Integer.toString(count)) ;
-        sb.append(Character.toString(s.charAt(n-1))) ;
-        return sb.toString() ; 
+        currString.append(count);
+        currString.append(ch);
+        return currString.toString();
     }
 }
