b42a90b4e6f4053227a1098112bc313cfc300013
MounishRamesh <mounishr6680@gmail.com>
2025-04-19 15:25:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (32.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,21 +3,15 @@ class Solution {
         int count = 0 ;
         int i = 0 ;
         int j = nums.length-1 ;
-        while(i <= j ){
+        while(i < j){
             int mid = i + ((j-i)/2) ;
-            System.out.print(mid + " ") ;
-            if((mid == 0 || nums[mid] > nums[mid-1]) && (mid == nums.length-1 || nums[mid] > nums[mid + 1])){
-                System.out.print("H") ;
-                return mid ;
-            }
-            else if(nums[mid] < nums[mid+1]){ 
+            if(nums[mid] < nums[mid+1]){
                 i = mid + 1 ;
-                 System.out.print(i + " sfgdskf");
             }
             else{
-                j = mid -1;
+                j = mid ;
             }
         }
-        return 0 ;
+        return i ;
     }
 }
