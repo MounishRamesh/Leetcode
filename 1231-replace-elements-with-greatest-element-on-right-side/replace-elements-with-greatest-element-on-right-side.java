309cb651d32108e8f065d637631eef040040559e
MounishRamesh <mounishr6680@gmail.com>
2025-04-20 14:53:14 +0530

    Time: 1 ms (99.95%) | Memory: 46.9 MB (34.86%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -1,14 +1,46 @@
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+//         for(int i = 0 ;i < arr.length-1 ;i ++){
+//             int max = arr[i+1] ;
+//             System.out.println(max + " ") ;
+//             for(int j = i+1 ;j < arr.length ;j ++){
+//                 max = Math.max(max, arr[j]) ;
+//             }
+//             arr[i] = max ;
+//         }
+//         arr[arr.length-1] = -1 ;
+//         return arr ;
+//     }
+// }
+
 class Solution {
-    public int[] replaceElements(int[] arr) {
-        for(int i = 0 ;i < arr.length-1 ;i ++){
-            int max = arr[i+1] ;
-            System.out.println(max + " ") ;
-            for(int j = i+1 ;j < arr.length ;j ++){
-                max = Math.max(max, arr[j]) ;
-            }
-            arr[i] = max ;
+
+    static {
+        for(int i = 0 ; i <= 3000 ; i++){
+        replaceElements(new  int[]{-1});
+
         }
-        arr[arr.length-1] = -1 ;
-        return arr ;
+    }
+    public static int[] replaceElements(int[] arr) {
+        
+      int len = arr.length - 1;
+      if(len == 0){
+        arr[0] = -1;
+      }else{
+      int max = arr[len];
+        arr[len] = -1;
+        for (int i = len - 1; i >= 0 ; i--) {
+            int t = arr[i];
+            arr[i] = max;
+            if (t > max) {
+                max =t;
+            }
+
+        }
+      }
+  
+
+        return arr;
+
     }
 }
