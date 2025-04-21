db925ee70111fa0e68932c278c66d30685b6325d
MounishRamesh <mounishr6680@gmail.com>
2025-04-21 10:41:56 +0530

    Time: 22 ms (40.99%) | Memory: 44.7 MB (28.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,22 +1,34 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s2.length() ;
-        for(int i = 0; i < n-s1.length()+1 ;i ++){
-            String s = s2.substring(i,s1.length()+i) ;
-            if(permutations(s, s1)){
+        int a = s1.length() ;
+        int b = s2.length() ;
+        if(a > b){
+            return false ;
+        }
+        HashMap<Character , Integer> hs = new HashMap<>() ;
+        HashMap<Character , Integer > hs1 = new HashMap<>() ;
+        char[] ch1 = s1.toCharArray() ;
+        for(char i : ch1){
+            hs.put(i , hs.getOrDefault(i,0)+1) ;
+        }
+        for(int i = 0 ;i < a ;i ++){
+            char ch = s2.charAt(i) ;
+            hs1.put(ch,hs1.getOrDefault(ch,0)+1) ;
+        }
+        if(hs.equals(hs1)){
+            return true ;
+        }
+        for(int i = a; i < b ;i ++){
+            char in = s2.charAt(i) ;
+            hs1.put(in,hs1.getOrDefault(in,0)+1) ;
+            char out = s2.charAt(i-a) ;
+            hs1.put(out,hs1.get(out)-1); 
+            if(hs1.get(out) == 0){
+                hs1.remove(out) ;
+            }
+            if(hs.equals(hs1)){
                 return true ;
             }
-        }
-        return false ;
-    }
-    public static boolean permutations(String s , String s1){
-        char[] arr = s.toCharArray() ;
-        char[] arr2 = s1.toCharArray() ;
-        Arrays.sort(arr) ;
-        Arrays.sort(arr2) ;
-        boolean ans = Arrays.equals(arr,arr2) ;
-        if(ans){
-            return true ;
         }
         return false ;
     }
