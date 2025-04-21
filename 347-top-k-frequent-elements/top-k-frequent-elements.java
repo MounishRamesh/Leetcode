f96205c4eaa4eaac196e4596905969e285d8aed0
MounishRamesh <mounishr6680@gmail.com>
2025-04-21 11:33:50 +0530

    Time: 271 ms (5.05%) | Memory: 48.7 MB (61.56%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,74 +1,74 @@
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         int[] arr = new int[k] ;
-//         List<Integer> ls = new ArrayList<>() ;
-//         List<Integer> ls1 = new ArrayList<>() ;
-//         HashMap<Integer , Integer> hs = new HashMap<>() ;
-//         for(int i : nums){
-//             hs.put(i ,hs.getOrDefault(i,0)+1) ;
-//         }
-//         for(int i : hs.values()){
-//             ls.add(i) ;
-//         }
-//         ls.sort(Collections.reverseOrder()) ;
-//         int count = 0 ;
-//         for(int i : ls){
-//             for(Map.Entry<Integer , Integer> entry: hs.entrySet()){
-//                 if(entry.getValue() == i && !(ls1.contains(entry.getKey()))){
-//                     ls1.add(entry.getKey()) ;
-//                     count ++ ;
-//                 }
-//                 if(count == k){
-//                     break ;
-//                 }
-//             }
-//             if(count == k){
-//                 break ;
-//             }
-//         }
-//         for(int i = 0 ;i < arr.length ;i ++){
-//             arr[i] = ls1.get(i) ;
-//             System.out.print(arr[i] + " ") ;
-//         }
-        
-//         return arr ;
-//     }
-// }
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
-        //hashmap to store frequencies
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
-
-        //create a bucket 
-        List<Integer>[] bucket = new List[nums.length+1];
-
-        //loop through array to add freq to hashmap
-        for(int n: nums){
-            freqMap.put(n,freqMap.getOrDefault(n,0)+1);
+        int[] arr = new int[k] ;
+        List<Integer> ls = new ArrayList<>() ;
+        List<Integer> ls1 = new ArrayList<>() ;
+        HashMap<Integer , Integer> hs = new HashMap<>() ;
+        for(int i : nums){
+            hs.put(i ,hs.getOrDefault(i,0)+1) ;
         }
-
-        for(int key: freqMap.keySet()){
-            int frequency = freqMap.get(key);
-            if(bucket[frequency] == null){
-               bucket[frequency] = new ArrayList<>(); 
-            }
-            bucket[frequency].add(key);
+        for(int i : hs.values()){
+            ls.add(i) ;
         }
-
-        int[] result = new int[k];
-        int counter =0;
-
-        for(int pos = bucket.length-1; pos>=0 && counter<k; pos--){
-            if(bucket[pos] != null){
-                for(Integer integer: bucket[pos]){
-                    result[counter++] = integer;
+        ls.sort(Collections.reverseOrder()) ;
+        int count = 0 ;
+        for(int i : ls){
+            for(Map.Entry<Integer , Integer> entry: hs.entrySet()){
+                if(entry.getValue() == i && !(ls1.contains(entry.getKey()))){
+                    ls1.add(entry.getKey()) ;
+                    count ++ ;
+                }
+                if(count == k){
+                    break ;
                 }
             }
+            if(count == k){
+                break ;
+            }
         }
-
-        return result;
+        for(int i = 0 ;i < arr.length ;i ++){
+            arr[i] = ls1.get(i) ;
+            System.out.print(arr[i] + " ") ;
+        }
         
+        return arr ;
     }
 }
+
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+
+//         //hashmap to store frequencies
+//         HashMap<Integer,Integer> freqMap = new HashMap<>();
+
+//         //create a bucket 
+//         List<Integer>[] bucket = new List[nums.length+1];
+
+//         //loop through array to add freq to hashmap
+//         for(int n: nums){
+//             freqMap.put(n,freqMap.getOrDefault(n,0)+1);
+//         }
+
+//         for(int key: freqMap.keySet()){
+//             int frequency = freqMap.get(key);
+//             if(bucket[frequency] == null){
+//                bucket[frequency] = new ArrayList<>(); 
+//             }
+//             bucket[frequency].add(key);
+//         }
+
+//         int[] result = new int[k];
+//         int counter =0;
+
+//         for(int pos = bucket.length-1; pos>=0 && counter<k; pos--){
+//             if(bucket[pos] != null){
+//                 for(Integer integer: bucket[pos]){
+//                     result[counter++] = integer;
+//                 }
+//             }
+//         }
+
+//         return result;
+        
+//     }
+// }
