e363a7098b8b68358adc6f5233867b772d938c91
MounishRamesh <mounishr6680@gmail.com>
2025-05-02 19:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (93.52%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,31 +1,47 @@
+// class Solution {
+//     public String largestGoodInteger(String num) {
+//         StringBuilder sb = new StringBuilder() ;
+//         ArrayList<Character> ls = new ArrayList<>() ;
+//         for(int i = 0 ;i < num.length()-2 ;i ++){
+//             if(isunique(num , i , i+2 )){
+//                 for(int j = i ; j < i+3 ;j ++){
+//                     ls.add(num.charAt(j)); 
+//                 }
+//             }
+//         }
+//         Collections.sort(ls) ;
+//         if(ls.size()>=3){
+//             for(int i = ls.size()-3 ; i < ls.size() ;i ++){
+//                 sb.append(ls.get(i)) ;
+//             }
+//         }
+//         return sb.toString() ;
+//     }
+//     public static boolean isunique(String num , int sta , int end){
+//         int i = sta ;
+//         int j = end ;
+//         while(i <= j){
+//             if(num.charAt(i) != num.charAt(j)){
+//                 return false ;
+//             }
+//             i ++ ;
+//         }
+//         return true ;
+//     }
+// }
+
+
 class Solution {
     public String largestGoodInteger(String num) {
-        StringBuilder sb = new StringBuilder() ;
-        ArrayList<Character> ls = new ArrayList<>() ;
-        for(int i = 0 ;i < num.length()-2 ;i ++){
-            if(isunique(num , i , i+2 )){
-                for(int j = i ; j < i+3 ;j ++){
-                    ls.add(num.charAt(j)); 
-                }
-            }
+       String[] numbers={"999","888","777","666","555","444","333","222"
+       ,"111","000"};
+       for(String s:numbers)
+       {
+        if(num.indexOf(s)>=0)
+        {
+            return s;
         }
-        Collections.sort(ls) ;
-        if(ls.size()>=3){
-            for(int i = ls.size()-3 ; i < ls.size() ;i ++){
-                sb.append(ls.get(i)) ;
-            }
-        }
-        return sb.toString() ;
-    }
-    public static boolean isunique(String num , int sta , int end){
-        int i = sta ;
-        int j = end ;
-        while(i <= j){
-            if(num.charAt(i) != num.charAt(j)){
-                return false ;
-            }
-            i ++ ;
-        }
-        return true ;
+       }
+       return "";
     }
 }
