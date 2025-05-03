a2506e1a23f45c83a519e4562876a7577402a60b
MounishRamesh <mounishr6680@gmail.com>
2025-05-03 10:46:28 +0530

    Time: 2 ms (100.00%) | Memory: 48.3 MB (32.70%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -44,36 +44,29 @@
 
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer , Integer> hs = new HashMap<>() ;
-        HashMap<Integer , Integer> first = new HashMap<>() ;
-        HashMap<Integer , Integer> last = new HashMap<>() ;
-        for(int i = 0 ; i < nums.length ; i++){
-            int num = nums[i];
-            hs.put(num , hs.getOrDefault(num,0)+1) ;
-            if(!first.containsKey(num)){
-                first.put(num, i);
-            }
-            last.put(num, i);
+        int max=0;
+        for(int z:nums){
+max=Math.max(max,z);
         }
-        int max = 0 ;
-        ArrayList<Integer> ls = new ArrayList<>() ;
-        for(int i : hs.keySet()){
-            int j = hs.get(i) ;
-            if(j > max){
-                ls.clear() ;
-                ls.add(i);
-                max = j ;
-            }
-            else if(j == max){
-                ls.add(i) ;
-            }
+       int[] count=new int[max+1];
+       int[] x=new int[max+1];
+       int[] y=new int[max+1];
+       int degree=0;
+      for(int i=0;i<nums.length;i++){
+        int num=nums[i];
+        count[num]++;
+degree=Math.max(degree,count[num]);
+        if(count[num]==1){
+            x[num]=i;
         }
-        int min = Integer.MAX_VALUE ;
-        for(int k = 0 ;k < ls.size() ;k ++){
-            int val = ls.get(k) ;
-            int len = last.get(val) - first.get(val) + 1;
-            min = Math.min(min, len);
-        }
-        return min ;
+        y[num]=i;
+    }
+    int min=nums.length;
+    for(int i=0;i<=max;i++){
+if(count[i]==degree){
+min=Math.min(min,y[i]-x[i]+1);
+}
+    }
+    return min;
     }
 }
