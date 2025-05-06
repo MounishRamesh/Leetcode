ccdb7586800f70775a92d1b9c0f6c975debc8a34
MounishRamesh <mounishr6680@gmail.com>
2025-05-06 19:35:08 +0530

    Time: 5 ms (81.59%) | Memory: 45.5 MB (60.70%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
@@ -1,16 +1,32 @@
+// class Solution {
+//     public int appendCharacters(String s, String t) {
+//         int i = 0 ;
+//         int j = 0 ;
+//         boolean ans = true ;
+//         while(i < s.length() && j < t.length()){
+//             char ch = s.charAt(i) ;
+//             char ch1 = t.charAt(j) ;
+//             if(ch == ch1){
+//                 j ++ ;
+//             }
+//             i ++ ;
+//         }
+//         return (t.length())-j ;
+//     }
+// }
+
 class Solution {
     public int appendCharacters(String s, String t) {
-        int i = 0 ;
-        int j = 0 ;
-        boolean ans = true ;
-        while(i < s.length() && j < t.length()){
-            char ch = s.charAt(i) ;
-            char ch1 = t.charAt(j) ;
-            if(ch == ch1){
-                j ++ ;
+        int pointerS = 0, pointerT = 0;
+        int sLength = s.length(), tLength = t.length();
+        
+        while (pointerS < sLength && pointerT < tLength) {
+            if (s.charAt(pointerS) == t.charAt(pointerT)) {
+                pointerT++;
             }
-            i ++ ;
+            pointerS++;
         }
-        return (t.length())-j ;
+        
+        return tLength - pointerT; 
     }
 }
