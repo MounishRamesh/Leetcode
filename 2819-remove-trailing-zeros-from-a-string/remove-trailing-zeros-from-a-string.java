50df8426b8661df92cd259006979e4383d60ca18
Mounish R <mounishr6680@gmail.com>
2025-06-20 12:21:51 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (27.33%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -4,16 +4,12 @@ class Solution {
         int ind = 0 ;
         for(int i = s.length()-1 ; i >= 0 ; i --){
             int a = (int)(s.charAt(i) - '0') ;
-            
-            System.out.println(a) ;
             if(a != 0){
                 ind = i ;
                 break ;
             }
         }
-        for(int i = 0 ; i <= ind ; i ++){
-            sb.append(s.charAt(i)) ;
-        }
-        return sb.toString() ;
+       
+        return s.substring(0,ind+1) ;
     }
 }
