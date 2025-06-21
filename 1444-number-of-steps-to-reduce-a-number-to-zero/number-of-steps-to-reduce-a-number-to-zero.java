ea160fe6b3e7eeaf8bf7655cd8e070ef8d11e436
Mounish R <mounishr6680@gmail.com>
2025-06-21 15:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (52.69%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,15 +1,32 @@
+// class Solution {
+//     public int numberOfSteps(int num) {
+//         int count = 0 ;
+//         while(num > 0){
+//             if(num % 2 == 0){
+//                 num /= 2 ;
+//             }
+//             else{
+//                 num = num-1 ;
+//             }
+//             count ++ ;
+//         }
+//         return count ;
+//     }
+// }
+
 class Solution {
     public int numberOfSteps(int num) {
-        int count = 0 ;
-        while(num > 0){
-            if(num % 2 == 0){
-                num /= 2 ;
+        int count =0;
+        while( num != 0){
+            if(num % 2 ==0){
+                count++;
+                num/=2;
             }
             else{
-                num = num-1 ;
+                count++;
+                num = num -1;
             }
-            count ++ ;
         }
-        return count ;
+        return count;
     }
 }
