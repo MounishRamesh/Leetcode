89b3325db5d1a144b96f8bfa7e1eede42cf70bd0
Mounish R <mounishr6680@gmail.com>
2025-06-21 14:32:33 +0530

    Time: 14 ms (9.12%) | Memory: 46.8 MB (33.55%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr = new int[n+1] ;
+        for(int i = 0; i < arr.length ;i ++){
+            int count = binary(i) ;
+            arr[i] = count ;
+        }
+        return arr ;
+    }
+    public static int binary(int i){
+        int count = 0 ;
+        while( i > 0){
+            int rem = i%2 ;
+            if(rem == 1){
+                count ++ ;
+            }
+            i/=2 ;
+        }
+        return count ;
+    }
+}
