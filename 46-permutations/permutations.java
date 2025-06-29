3932775b9fc3cdb2fdb2cef55083f28bf50aeca8
Mounish R <mounishr6680@gmail.com>
2025-06-29 16:36:39 +0530

    Time: 1 ms (93.14%) | Memory: 44.8 MB (30.40%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ls = new ArrayList<>() ;
-        allPermutations(nums , 0 , ls) ;
-        return ls;
+        permute(nums , 0 , ls) ;
+        return ls ;
     }
-    public static void allPermutations(int[] nums , int i , List<List<Integer>> ls){
+    public static void permute(int[] nums , int i , List<List<Integer>> ls){
         if(i == nums.length-1){
             List<Integer> ls1 = new ArrayList<>() ;
-            for(int i1 : nums){
-                ls1.add(i1) ;
+            for(int num : nums){
+                ls1.add(num) ;
             }
             ls.add(ls1) ;
-            return ;
         }
         for(int j = i ; j < nums.length ; j ++){
-            isswap(nums , j , i) ;
-            allPermutations(nums , i+1 , ls) ;
-            isswap(nums , j , i) ;
+            swap(nums , j , i) ;
+            permute(nums , i+1 , ls) ;
+            swap(nums , j , i) ;
         }
     }
-    public static int[] isswap(int[] nums , int j , int i){
-        int tem = nums[j] ;
-        nums[j] = nums[i] ;
-        nums[i] = tem ;
+    public static int[] swap(int[] nums , int i , int j){
+        int tem = nums[i] ;
+        nums[i] = nums[j] ;
+        nums[j] = tem ;
         return nums ;
     }
 }
