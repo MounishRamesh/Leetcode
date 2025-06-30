a76755aaf1feded8d53c77b44ca3cb915a7afd71
Mounish R <mounishr6680@gmail.com>
2025-06-30 17:16:42 +0530

    Time: 7 ms (4.70%) | Memory: 41.1 MB (16.29%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,13 +1,14 @@
 class Solution {
-    public static int trailingZeroes(int n) {
+    public int trailingZeroes(int n) {
+        int val = factorail(n , 0) ;
+        return val ;
+    }
+    public static int factorail(int n , int sum){
+        System.out.println(sum) ;
         if(n < 5){
-            return 0 ;
+            return sum ;
         }
-        int sum = 0 ;
-        while(n >= 5){
-            sum = sum + n / 5 ;
-            n = n / 5; 
-        }
-        return sum ;
+        sum += n/5 ;
+        return factorail(n/5 , sum) ;
     }
 }
