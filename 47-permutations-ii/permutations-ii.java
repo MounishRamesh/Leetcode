f5438b8cd4b59e976de62daab32415bda104504e
Mounish R <mounishr6680@gmail.com>
2025-07-01 23:09:34 +0530

    Time: 24 ms (24.84%) | Memory: 46 MB (6.44%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ls1 = new ArrayList<>() ;
+        Set<List<Integer>> ls1 = new HashSet<>() ;
+        List<List<Integer>> ls2 = new ArrayList<>() ;
         pmu(nums , 0 , ls1) ;
-        return ls1 ;
+        for(List<Integer> i : ls1){
+            ls2.add(i) ;
+        }
+        return ls2 ;
     }
-    public static void pmu(int[] nums , int i , List<List<Integer>> ls1){
+    public static void pmu(int[] nums , int i , Set<List<Integer>> ls1){
         if(i == nums.length-1){
             List<Integer> ls = new ArrayList<>() ;
             for(int j : nums){
                 ls.add(j) ;
             }
-            if(isvalid(ls , ls1)){
-                ls1.add(ls) ;
-            }
+            ls1.add(ls) ;
             return ;
         }
         for(int k = i ; k < nums.length ;k ++){
@@ -25,13 +27,5 @@ class Solution {
         int temp = nums[i] ;
         nums[i] = nums[j] ;
         nums[j] = temp ;
-    }
-    public static boolean isvalid(List<Integer> ls , List<List<Integer>> ls1){
-
-        if(ls1.contains(ls)){
-            System.out.println(ls) ;
-            return false ;
-        }
-        return true ;
     }
 }
