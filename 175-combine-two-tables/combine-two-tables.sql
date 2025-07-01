1bc9a1db4a3673047628f8c4515866ed6c2b7d63
Mounish R <mounishr6680@gmail.com>
2025-07-01 12:43:33 +0530

    Time: 460 ms (18.88%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,11 @@
+-- # Write your MySQL query statement below
+-- select p.firstname , p.lastname , a.city , a.state
+-- from person as p
+-- left join address as a
+-- on a.personId = p.personId;
+
 # Write your MySQL query statement below
-select p.firstname , p.lastname , a.city , a.state
-from person as p
-left join address as a
-on (a.personId = p.personId) ;
+SELECT firstName, lastName, city, state
+FROM Person
+LEFT JOIN Address 
+ON Person.personID = Address.personID
