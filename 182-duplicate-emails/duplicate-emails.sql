5e5274fc208e0048de20a31b3e668903c5fbcb86
Mounish R <mounishr6680@gmail.com>
2025-07-02 11:47:47 +0530

    Time: 328 ms (97.38%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,11 @@
-# Write your MySQL query statement below
-SELECT Email 
-FROM person 
-group by Email
-Having count(*) > 1 ;
+-- # Write your MySQL query statement below
+-- SELECT Email 
+-- FROM person 
+-- group by Email
+-- Having count(*) > 1 ;
+
+
+select email
+from person
+group by email
+having count(*)>1
