e765bb48b6edfd848559b14746b1c9b7d309e61d
Mounish R <mounishr6680@gmail.com>
2025-07-14 14:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (53.29%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -18,43 +18,12 @@ class Solution {
         if(p == null && q == null){
             return true ;
         }
-        if((p == null || q == null) && (p!= null || q != null)){
+        if(p == null ||q== null){
+            return false;
+        }
+        if(p.val != q.val){
             return false ;
         }
-        Queue<TreeNode> q1 = new LinkedList<>() ;
-        Queue<TreeNode> q2 = new LinkedList<>() ;
-        q1.add(p) ;
-        q2.add(q) ;
-        while(!q1.isEmpty() && !q2.isEmpty()){
-            TreeNode node1 = q1.poll() ;
-            TreeNode node2 = q2.poll() ;
-            System.out.println(node1.val + " " + node2.val ) ;
-            if(node1.val != node2.val){
-                // System.out.println(node1.val + " " + node2.val ) ;
-                return false ;
-            }
-            if(node1.left != null && node2.left != null){
-                q1.add(node1.left) ;
-                q2.add(node2.left) ;
-            }
-            if(node1.right != null && node2.right != null){
-                q1.add(node1.right) ;
-                q2.add(node2.right) ;
-            }
-            if(node1.left == null && node2.left != null){
-                return false ;
-            }
-            if(node1.right == null && node2.right != null){
-                return false ;
-            }
-            if(node1.right != null && node2.right == null){
-                return false ;
-            }
-            if(node1.left != null && node2.left == null ){
-                return false ;
-            }
-
-        }
-        return true ;
+        return ((isSameTree(p.left , q.left)) && (isSameTree(p.right , q.right))) ;
     }
 }
