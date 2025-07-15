b76db6cc83947cc085896bff2d798ae89754edca
Mounish R <mounishr6680@gmail.com>
2025-07-15 14:13:03 +0530

    Time: 12 ms (97.06%) | Memory: 47 MB (24.18%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -3,9 +3,7 @@ class Solution {
         int count = 0 ;
         Arrays.sort(nums) ;
         for(int i = 0 ;i < nums.length ;i +=2){
-            int max = Math.min(nums[i] , nums[i+1] ) ;
-            //System.out.println(max) ;
-            count += max ;
+            count += nums[i] ;
         }
         return count ;
     }
