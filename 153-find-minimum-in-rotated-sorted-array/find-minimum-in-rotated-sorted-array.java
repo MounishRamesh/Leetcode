fc036628f21c604dd619422e26a78bfc22c5d423
Mounish R <mounishr6680@gmail.com>
2025-07-20 23:46:19 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (73.96%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -3,20 +3,39 @@ class Solution {
         int i = 0 ;
         int j = nums.length-1 ;
         int min = Integer.MAX_VALUE ;
-        while(i <= j){
+        while(i < j){
             int mid = i+(j-i)/2 ;
-            System.out.println(mid) ;
-            if(nums[i] < nums[mid]){
-                min = Math.min(min , nums[i]) ;
-                i = mid +1 ;
+            //System.out.println(mid) ;
+            if (nums[mid] > nums[j]) {
+                i = mid + 1;
             }
             // System.out.println(nums[mid]) ;
             else{
-                int min1 = Math.min(nums[mid] , nums[j]) ;
-                min = Math.min(min , min1) ;
-                j = mid-1 ;
+                j = mid ;
             }
         }
-        return min ;
+        return nums[i] ;
     }
 }
+
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int i = 0;
+//         int j = nums.length - 1;
+
+//         while (i < j) {
+//             int mid = i + (j - i) / 2;
+
+//             // If mid element is greater than the last element,
+//             // the min must be in the right half
+//             if (nums[mid] > nums[j]) {
+//                 i = mid + 1;
+//             } else {
+//                 // The min is at mid or to the left
+//                 j = mid;
+//             }
+//         }
+//         // At the end, both i and j will point to the minimum
+//         return nums[i];
+//     }
+// }
