17adbe9651034273864cfb2ce24cf4df7d8e3250
Mounish R <mounishr6680@gmail.com>
2025-08-01 17:03:21 +0530

    Time: 5 ms (2.51%) | Memory: 42.4 MB (8.53%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,46 +1,46 @@
-// class Solution {
-//     public int countHillValley(int[] nums) {
-//         int count = 0 ;
-//         if(nums.length <= 2){
-//             return 0 ;
-//         }
-//         List<Integer> ls = new ArrayList<>() ;
-//         int ind = 0 ;
-//         for(int i = 0 ;i < nums.length-1 ;i ++){
-//             if(nums[i] != nums[i+1]){
-//                 ls.add(nums[i]) ;
-//                 ind = i ;
-//             }
-//         }
-//         if(nums[ind] != nums[nums.length-1]){
-//             ls.add(nums[nums.length-1]) ;
-//         }
-//         for(int i = 1 ; i < ls.size()-1 ; i ++){
-//             if(ls.get(i-1) > ls.get(i) && ls.get(i+1) > ls.get(i)){
-//                 count ++ ;
-//             }
-//             else if(ls.get(i-1) < ls.get(i) && ls.get(i+1) < ls.get(i)){
-//                 count ++ ;
-//             }
-//         }
-//         System.out.println(ls) ;
-//         return count ;
-//     }
-// }
-
 class Solution {
     public int countHillValley(int[] nums) {
-        int count=0;
-        int left=0;
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1]){
-                if((nums[i]>nums[left] && nums[i]>nums[i+1])||
-                (nums[i]<nums[left]&& nums[i]<nums[i+1])){
-                    count ++;
-                }
-                left=i;
+        int count = 0 ;
+        if(nums.length <= 2){
+            return 0 ;
+        }
+        List<Integer> ls = new ArrayList<>() ;
+        int ind = 0 ;
+        for(int i = 0 ;i < nums.length-1 ;i ++){
+            if(nums[i] != nums[i+1]){
+                ls.add(nums[i]) ;
+                ind = i ;
             }
         }
-        return count;
+        if(nums[ind] != nums[nums.length-1]){
+            ls.add(nums[nums.length-1]) ;
+        }
+        for(int i = 1 ; i < ls.size()-1 ; i ++){
+            if(ls.get(i-1) > ls.get(i) && ls.get(i+1) > ls.get(i)){
+                count ++ ;
+            }
+            else if(ls.get(i-1) < ls.get(i) && ls.get(i+1) < ls.get(i)){
+                count ++ ;
+            }
+        }
+        System.out.println(ls) ;
+        return count ;
     }
 }
+
+// class Solution {
+//     public int countHillValley(int[] nums) {
+//         int count=0;
+//         int left=0;
+//         for(int i=1;i<nums.length-1;i++){
+//             if(nums[i]!=nums[i+1]){
+//                 if((nums[i]>nums[left] && nums[i]>nums[i+1])||
+//                 (nums[i]<nums[left]&& nums[i]<nums[i+1])){
+//                     count ++;
+//                 }
+//                 left=i;
+//             }
+//         }
+//         return count;
+//     }
+// }
