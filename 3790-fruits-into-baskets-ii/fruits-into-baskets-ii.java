d6cabd29a8ece8b7e52594c8548ad929e7e0b1d9
Mounish R <mounishr6680@gmail.com>
2025-08-05 19:06:53 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (80.83%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,26 +1,16 @@
 class Solution {
-    public int numOfUnplacedFruits(int[] arr, int[] baskets) {
-        List<Integer> ls = new ArrayList<>() ;
-        for(int i : baskets){
-            ls.add(i) ;
-        }
+    public int numOfUnplacedFruits(int[] fruits, int[] arr) {
         int count = 0 ;
-        for(int i = 0 ; i< arr.length ; i ++){
-            boolean ans = false ;
-            int max = Integer.MIN_VALUE ;
-            for(int j = 0 ; j < ls.size() ; j ++){
-                max = Math.max(max , ls.get(j)) ;
-                if(ls.get(j) >= arr[i]){
-                    ls.remove(j) ;
-                    ans = true ;
+        for(int i = 0 ; i < arr.length ; i ++){
+            boolean ans = true ;
+            for(int j = 0 ; j < arr.length ; j ++){
+                if(fruits[i] <= arr[j]){
+                    count ++ ;
+                    arr[j] = 0 ;
                     break ;
-                    // max = Math.max(max , ls.get(j)) ;
                 }
             }
-            if(!ans){
-                count ++ ;
-            }
         }
-        return count ;
+        return fruits.length-count ;
     }
 }
