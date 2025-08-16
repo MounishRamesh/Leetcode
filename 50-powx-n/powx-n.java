ffb1fc19a9ef2985f073914dfd4eff046e49dc70
Mounish R <mounishr6680@gmail.com>
2025-08-16 09:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (61.58%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,23 +1,52 @@
+// class Solution {
+//     public double myPow(double x, int n) {
+//         long N = n ;
+//         if(n < 0){
+//             N = -N ;
+//             x = 1/x ;
+//         }
+//         return value(x,N) ;
+//     }
+//     public static double value(double x , Long N){
+//         if(N == 0){
+//             return 1.0 ;
+//         }
+//         double half = value(x , N/2) ;
+//         if(N%2 == 0){
+//             return half * half ;
+//         }
+//         else{
+//              return half * half * x ;
+//         }
+
+//     }
+// }
+
 class Solution {
     public double myPow(double x, int n) {
-        long N = n ;
-        if(n < 0){
-            N = -N ;
-            x = 1/x ;
-        }
-        return value(x,N) ;
-    }
-    public static double value(double x , Long N){
-        if(N == 0){
-            return 1.0 ;
-        }
-        double half = value(x , N/2) ;
-        if(N%2 == 0){
-            return half * half ;
-        }
-        else{
-             return half * half * x ;
-        }
+        if(n == 0) return 1.0;
+        else if(x == 0.0) return 0.0;
 
+        double cur = pow(x, Math.abs((long)n));
+        // System.out.println(cur);
+        if(n < 0){
+            return 1.0 / cur;
+        }else{
+            return cur;
+        }
+    }
+
+    private double pow(double x, long n){
+        double res = 1;
+        while(n > 0){
+            if((n & 1) == 1){
+                res *= x;
+                n--;
+            }
+            x = (x * x);
+            n /= 2;
+
+        }
+        return res;
     }
 }
