192ee7cdeac8335739806083d1edeeaaeb683943
Mounish R <mounishr6680@gmail.com>
2025-08-16 08:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (44.60%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,36 +1,17 @@
 class Solution {
     public int maximum69Number (int num) {
-        int max = num ;
-        int tem = num ;
-        int i = 0 ;
-        while(num > 0){
-            String s = Integer.toString(tem) ;
-            int a = toconvert(s , i) ;
-            // break ;
-            max = Math.max(a , max) ;
-            i ++ ;
-            num = num/10 ;
-           // break ;
-        }
-        return max ;
-    }
-    public static int toconvert(String s , int i) {
         StringBuilder sb = new StringBuilder() ;
-        for(int j = 0  ; j < s.length() ; j ++){
-            char ch = s.charAt(j) ;
-            if(i == j){
-                if(ch == '6'){
-                    sb.append('9') ;
-                }
-                else{
-                    sb.append('6') ;
-                }
+        sb.append(num) ;
+        boolean check = false ;
+        int i = 0 ;
+        while(!check && i < sb.length()){
+            char ch = sb.charAt(i) ;
+            if(ch == '6'){
+                sb.setCharAt(i , '9');
+                check= true; 
             }
-            else{
-                sb.append(ch) ;
-            }
+            i ++ ;
         }
-        System.out.println(sb.toString()) ;
-        return (Integer.parseInt(sb.toString())) ;
+        return Integer.parseInt(sb.toString()); 
     }
 }
