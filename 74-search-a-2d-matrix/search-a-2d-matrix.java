9072dae918fedf6247dfad6c79a6ef309da08a51
Mounish R <mounishr6680@gmail.com>
2025-09-03 15:30:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (10.40%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -5,18 +5,11 @@ class Solution {
         int[][] arr ;
         int l = 0 ;
         int r = (m*n)-1 ;
-        // if(m == 1 && n ==1 ){
-        //     if(matrix[0][0] == target){
-        //         return true ;
-        //     }
-        //     else{
-        //         return false ;
-        //     }
-        // }
+        
         while(l <= r){
             int mid = (l)+(r-l)/2 ;
             int val = matrix[mid/n][mid%n]; 
-            System.out.println(mid + " " + val + " " + l + " " + r); 
+            // System.out.println(mid + " " + val + " " + l + " " + r); 
             if(val == target){
                 return true ;
             }
