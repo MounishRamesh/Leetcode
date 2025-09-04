7e5d56e7d0b2a41eb93a914152ff2019e99fae8f
Mounish R <mounishr6680@gmail.com>
2025-09-04 19:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (71.18%) - LeetSync

--- 3830-find-closest-person/find-closest-person.java
+++ 3830-find-closest-person/find-closest-person.java
@@ -1,15 +1,35 @@
+// class Solution {
+//     public int findClosest(int x, int y, int z) {
+//         int count1 = Math.abs(x-z) ;
+//         int count2 = Math.abs(y-z) ;
+//         if(count1 == count2){
+//             return 0 ;
+//         }
+//         else if(count1 < count2){
+//             return 1 ;
+//         }
+//         else{
+//             return 2 ;
+//         }
+//     }
+// }
+
 class Solution {
     public int findClosest(int x, int y, int z) {
-        int count1 = Math.abs(x-z) ;
-        int count2 = Math.abs(y-z) ;
-        if(count1 == count2){
-            return 0 ;
+        int distanceX = Math.abs(z-x);
+        int distanceY = Math.abs(z-y);
+
+        if(distanceX > distanceY)
+        {
+            return 2;
         }
-        else if(count1 < count2){
-            return 1 ;
+        else if(distanceX == distanceY)
+        {
+            return 0;
         }
-        else{
-            return 2 ;
+        else
+        {
+            return 1;
         }
     }
 }
