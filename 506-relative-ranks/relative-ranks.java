a61eab042029610356f9e595bc509fb2c4f99c1a
Mounish R <mounishr6680@gmail.com>
2025-09-06 21:00:05 +0530

    Time: 9 ms (59.98%) | Memory: 46.2 MB (5.46%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,32 +1,30 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        int[] arr = new int[score.length] ;
-        for(int i = 0 ; i < score.length ;i ++){
-            int count = 1 ;
-            for(int j = 0 ; j < score.length ;j ++){
-                if(i != j){
-                    if(score[i] < score[j]){
-                        count ++ ;
-                    }
-                }
-            }
-            arr[i] = count ;
+        int[] arr = score.clone() ;
+        HashMap<Integer , Integer> hs = new HashMap<>() ;
+        Arrays.sort(score) ;
+        int ran = 1 ;
+        for(int i = score.length-1 ; i >= 0 ; i --){
+            int val = score[i] ;
+            hs.put(val , hs.getOrDefault(val , ran++)) ;
         }
+        // System.out.println(hs) ;
         String[] a = new String[score.length] ;
-        for(int i = 0 ; i < a.length; i ++){
-            String s = String.valueOf(arr[i]) ;
-            if(s.equals("1")){
-                a[i] = "Gold Medal" ;
-            }
-            else if(s.equals("2")){
-                a[i] = "Silver Medal" ;
-            }
-            else if(s.equals("3")){
-                a[i] = "Bronze Medal" ;
-            }
-            else{
-                a[i] = s ;
-            }
+        for(int i = 0 ; i < arr.length ;i ++){
+           String ans = String.valueOf(hs.get(arr[i])) ;
+        //    System.out.println(ans) ;
+           if(ans.equals("1")){
+              a[i] = "Gold Medal" ;
+           }
+           else if(ans.equals("2")){
+             a[i] = "Silver Medal" ;
+           }
+           else if(ans.equals("3")){
+              a[i] = "Bronze Medal" ;
+           }
+           else{
+            a[i] = ans ;
+           }
         }
         return a ;
     }
