b8c0cf9c990141d761a2acfc454d2b0f20005e8d
Mounish R <mounishr6680@gmail.com>
2025-09-18 22:38:25 +0530

    Time: 12 ms (5.16%) | Memory: 43.8 MB (5.16%) - LeetSync

--- 1698-replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.java
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.java
@@ -6,20 +6,17 @@ class Solution {
         String[] arr = s.split("") ;
         List<String> ls = new ArrayList<>() ;
         for(int i = 0 ; i < arr.length ;i ++){
-            
             if(arr[i].equals("?")){
                 if(i == 0){
                     ls.add(arr[i+1]) ;
                 }
                 else if(i == arr.length-1){
                     ls.add(arr[arr.length-2]) ;
-                    // System.out.println(ls + " " ) ;
                 }
                 else{
                     ls.add(arr[i-1]) ;
                     ls.add(arr[i+1]) ;
                 }
-                // System.out.println(ls + " " + i) ;
                 for(int j = 0 ; j < 27 ; j ++){
                     char ch = (char)(j+'a') ;
                     if(!ls.contains(String.valueOf(ch))){
@@ -28,7 +25,6 @@ class Solution {
                         break ;
                     }
                 }
-                // System.out.println(ls) ;
             }
             ls.clear(); 
         }
