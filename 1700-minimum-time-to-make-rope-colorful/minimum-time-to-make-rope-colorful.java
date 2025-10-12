84d378750e4474cc6cda566a937791e4bcf3297b
Mounish R <mounishr6680@gmail.com>
2025-10-12 10:19:18 +0530

    Time: 9 ms (57.14%) | Memory: 60.6 MB (72.63%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,33 +1,12 @@
 class Solution {
     public int minCost(String c, int[] n) {
-        List<Integer> ls = new ArrayList<>() ;
         int count = 0 ;
-        int sum = 0 ;
-        for(int i = 0 ; i < c.length()-1 ; i ++ ){
-            char ch = c.charAt(i) ;
-            char ch1 = c.charAt(i+1) ;
-            if(ch == ch1){
-                ls.add(n[i]) ;
-                sum += n[i] ;
-            }
-            else{
-                ls.add(n[i]) ;
-                sum += n[i] ;
-                Collections.sort(ls) ;
-                int rem = ls.get(ls.size()-1) ;
-                sum -= rem ;
-                count += sum ;
-                
-                sum = 0 ;
-                ls.clear() ;
+        for(int i = 1 ; i < c.length() ; i ++){
+            if(c.charAt(i) == c.charAt(i-1)){
+                count += Math.min(n[i] , n[i-1]) ;
+                n[i] = Math.max(n[i] , n[i-1]) ;
             }
         }
-        ls.add(n[c.length()-1]) ;
-        sum += n[c.length()-1] ;
-        Collections.sort(ls) ;
-        int rem = ls.get(ls.size()-1) ;
-        sum -= rem ;
-        count += sum ;
         return count ;
     }
 }
