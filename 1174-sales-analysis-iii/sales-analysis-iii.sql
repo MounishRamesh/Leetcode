9306a966535a1d37aa7b95c79894e39f064e4b1d
Mounish R <mounishr6680@gmail.com>
2025-10-18 10:13:31 +0530

    Time: 1082 ms (70.12%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,13 +1,7 @@
-SELECT p.product_id , p.product_name
-FROM Product AS p
-WHERE p.product_id IN (
-    SELECT s.product_id
-    FROM Sales AS s
-    WHERE s.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
-    GROUP BY s.product_id
-    HAVING COUNT(*) = (
-        SELECT COUNT(*)
-        FROM Sales AS s2
-        WHERE s2.product_id = s.product_id
-    )
-);
+# Write your MySQL query statement below
+SELECT p.product_id , p.product_name from Product as p
+join Sales as s
+on p.product_id = s.product_id 
+group by p.product_id 
+HAVING min(s.sale_date) >= '2019-01-01' and
+max(s.sale_date) <= '2019-03-31' ;
